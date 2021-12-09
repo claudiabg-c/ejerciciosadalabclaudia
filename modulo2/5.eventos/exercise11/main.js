57a66b3124b8a6501b61300f63e430a8cca6fd02
Claudia <claudia.berrocalgomez@gmail.com>
2021-12-09 23:01:41 +0100

    add homework

--- modulo2/5.eventos/exercise11/main.js
+++ modulo2/5.eventos/exercise11/main.js
@@ -1,2 +1,9 @@
 'use strict'
 
+const link1 = document.querySelector('.link1');
+const link2 = document.querySelector('.link2');
+const link3 = document.querySelector('.link3');
+
+link1.addEventListener ('click', (event) => event.preventDefault());
+link2.addEventListener ('click', (event) => event.preventDefault());
+link3.addEventListener ('click', (event) => event.preventDefault());
