eb6876d424b99c585a2b7e115b289d55eae4ebf5
Claudia <claudia.berrocalgomez@gmail.com>
2021-12-13 23:29:19 +0100

    add homework

--- modulo2/6.objetos/main.js
+++ modulo2/6.objetos/main.js
@@ -8,42 +8,90 @@ const adalaber = {
     job: 'periodista',
 };
 
-console.log(`Mi nombre es ${adalaber.name}, tengo ${adalaber.age}, y soy ${adalaber.job}`);
+const adalaber2 = {
+    name: 'Rocío',
+    age: '25 años',
+    job: 'actriz'
+};
+
+console.log(`Mi nombre es ${adalaber.name}, tengo ${adalaber.age}, y soy ${adalaber.job}.`);
+
+console.log(`Mi nombre es ${adalaber2.name}, tengo ${adalaber2.age}, y soy ${adalaber2.job}.`);
 
 //exercise 2
 
-adalaber.run = function () {
-    return 'Estoy corriendo';
-};
+adalaber.run = phrase => `${phrase}`;
 
-console.log(adalaber.run());
+adalaber.runAMarathon = distance => `${adalaber.run('Estoy corriendo')} un maratón de ${distance} kilómetros.`
+
+console.log(adalaber.runAMarathon(50));
 
 //exercise 3
 
 adalaber.showBio = function() {
-    console.log(`Mi nombre es ${this.name}, tengo ${this.age}, y soy ${this.job}`);
+    return 'Mi nombre es ' + this.name + ', tengo ' + this.age + ', y soy ' + this.job;
+};
+
+adalaber2.showBio = function() {
+    return 'Mi nombre es ' + this.name + ', tengo ' + this.age + ', y soy ' + this.job;
+};
+
+console.log(adalaber.showBio());
+console.log(adalaber2.showBio());
+
+//exercise 4
+
+const button = document.querySelector('.button');
+
+function showInfo(event) {
+    console.log(event);
 }
 
-adalaber.showBio();
+button.addEventListener('click', showInfo);
 
+//exercise 5
+
+const input = document.querySelector('.input');
+
+console.dir(input);
 
 //exercise 6
+
 const pearsBasket = {
     max: 18,
     min: 13,
     current: 14,
     initial: 0,
-}
+};
 
-const pearsBasket2 = {};
-
-pearsBasket2.max = 11;
-pearsBasket2.min = 5;
-pearsBasket2.current = 6;
-pearsBasket2.initial = 0;
-
-pearsBasket2.add = function () {
+pearsBasket.add = function() {
     this.current++;
-}
+};
 
-pearsBasket2.add();
+pearsBasket.remove = function() {
+    this.current--;
+};
+
+pearsBasket.add();
+pearsBasket.remove();
+
+console.log(pearsBasket);
+
+//exercise 7
+
+const job = 'developer';
+
+const user = {};
+
+user["firstName"] = 'Pepe';
+user["lastName"] = 'Pérez';
+user["age"] = 30;
+user["job"] = `${job}`;
+
+const changeUser = user;
+
+changeUser.firstName = 'Agustín';
+
+user.age += 1;
+
+console.log(user);
