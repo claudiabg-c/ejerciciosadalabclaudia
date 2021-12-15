7edfd894a64c0b3211ad1c75e506cbc23e20d32b
Claudia <claudia.berrocalgomez@gmail.com>
2021-12-15 19:56:26 +0100

    add homework

--- modulo2/8.arrays2/main.js
+++ modulo2/8.arrays2/main.js
@@ -2,18 +2,27 @@
 
 //exercise 1
 
-const numbers1to100 = [get100Numbers()];
+const numbers1to100 = [];
 
 function get100Numbers() {
-    for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    };
+    let addNumbers = 0;
+
+    for (let numbers = 1; numbers <= 100; numbers++) {
+    addNumbers = numbers1to100.push(numbers);
+    }
+    
+    console.log(numbers1to100);
 };
 
-//exercise 2 ------ PENDIENTE DE TERMINAR
+get100Numbers();
+
+
+//exercise 2
 
 function getReversed100Numbers() {
     console.log(numbers1to100.reverse());
 };
 
 getReversed100Numbers();
+
+
