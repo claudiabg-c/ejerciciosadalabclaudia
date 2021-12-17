5b9065ed6b5c9a27bb8477af86c10bb28ba10d84
Claudia <claudia.berrocalgomez@gmail.com>
2021-12-17 20:54:56 +0100

    add homework

--- modulo2/10.peticiones/exercise6/main.js
+++ modulo2/10.peticiones/exercise6/main.js
@@ -11,15 +11,12 @@ function randomDog() {
     .then((responseDog) => responseDog.json())
     .then((dataDog) => {
       chosenBreed = dataDog.message;
-      console.log(chosenBreed);
       return fetch("https://api.rand.fun/number/integer?max=94");
     })
     .then((responseNumber) => responseNumber.json())
     .then((dataNumber) => {
       const chosenNumber = dataNumber.result;
-      console.log(chosenNumber);
       const giveRandomDog = chosenBreed[chosenNumber];
-      console.log(giveRandomDog);
       dogBreed.innerHTML = `${giveRandomDog}`;
       return fetch(`https://dog.ceo/api/breed/${giveRandomDog}/images/random`);
     })
