b99b8fed0e97cd225784474df27e98e0784b16e6
Claudia <claudia.berrocalgomez@gmail.com>
2022-01-13 16:21:21 +0100

    add exercises module 3 lesson 4

--- modulo3/04.Arrays/01.to-do-list/src/components/App.js
+++ modulo3/04.Arrays/01.to-do-list/src/components/App.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import "../styles/App.scss";
 
 function App() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { task: "Comprar harina, jamón y pan rallado", completed: true },
     { task: "Hacer croquetas ricas", completed: true },
     { task: "Ir a la puerta de un gimnasio", completed: false },
@@ -9,12 +10,30 @@ function App() {
       task: "Comerme las croquetas mirando a la gente que entra en el gimnasio",
       completed: false,
     },
-  ];
+  ]);
+
+  for (let index = 0; index < tasks.length; index++) {
+    tasks[index].id = `${[index]}`;
+  }
+
+  console.log(tasks);
+
+  const handleClick = (event) => {
+    const clickedTask = event.currentTarget.id;
+    const foundTask = tasks.find((task) => task.id === clickedTask);
+    foundTask.completed = !foundTask.completed;
+    setTasks([...tasks]);
+  };
 
   const renderList = () => {
     return tasks.map((task, index) => {
       return (
-        <li key={index} className={`${task.completed}`}>
+        <li
+          key={index}
+          id={index}
+          className={`${task.completed}`}
+          onClick={handleClick}
+        >
           {task.task}
         </li>
       );
