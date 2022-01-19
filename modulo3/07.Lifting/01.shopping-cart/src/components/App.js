ab7579213e33269173945fd2975474f087ac0454
Claudia <claudia.berrocalgomez@gmail.com>
2022-01-19 20:25:18 +0100

    add exercises module 3 lesson 7

--- modulo3/07.Lifting/01.shopping-cart/src/components/App.js
+++ modulo3/07.Lifting/01.shopping-cart/src/components/App.js
@@ -1,10 +1,6 @@
 import { useState } from "react";
-import InputGroupCheck from "./InputGroupCheck";
-import InputGroupRadio from "./InputGroupRadio";
-import InputGroupSelect from "./InputGroupSelect";
-import InputGroupText from "./InputGroupText";
 import Preview from "./Preview";
-import Button from "./Button";
+import Form from "./Form";
 
 const App = () => {
   const [name, setName] = useState("");
@@ -54,88 +50,29 @@ const App = () => {
     setLegalTerms(false);
   };
 
-  const handleForm = (ev) => {
-    ev.preventDefault();
-    console.log("Enviando datos al servidor...");
-  };
-
   return (
     <div>
-      <form className="form" onSubmit={handleForm}>
-        <h2>Rellena tus datos para finalizar la compra:</h2>
-        <div className="form">
-          <InputGroupText
-            id="name"
-            text="Escribe un nombre:"
-            type="text"
-            placeholder="María García"
-            value={name}
-            handleInput={handleName}
-          />
-          <InputGroupText
-            id="email"
-            text="Escribe un email:"
-            type="email"
-            placeholder="mariagarcia@gmail.com"
-            value={email}
-            handleInput={handleEmail}
-          />
-          <InputGroupSelect
-            id="region"
-            text="Indica tu región:"
-            value={region}
-            handleSelect={handleRegion}
-          />
-          <label className="label-text">Indica tu método de pago:</label>
-          <InputGroupRadio
-            value="creditCard"
-            text="Tarjeta de crédito"
-            checked={paymentType === "creditCard"}
-            handleRadio={handlePaymentType}
-          />
-          <InputGroupRadio
-            value="cash"
-            text="Efectivo"
-            checked={paymentType === "cash"}
-            handleRadio={handlePaymentType}
-          />
-          <InputGroupRadio
-            value="cashOnDelivery"
-            text="Contra reembolso"
-            checked={paymentType === "cashOnDelivery"}
-            handleRadio={handlePaymentType}
-          />
-          <InputGroupCheck
-            id="legalTerms"
-            text="Debes aceptar nuestros términos legales para completar la compra:"
-            checked={legalTerms}
-            handleCheck={handleLegalTerms}
-          />
-        </div>
-        <Preview
-          paymentType={paymentType}
-          name={name}
-          email={email}
-          region={region}
-          legalTerms={legalTerms}
-        />
-        <Button
-          class="button"
-          text="Enviar"
-          type="submit"
-          name={name}
-          email={email}
-          paymentType={paymentType}
-          legalTerms={legalTerms}
-          disabled={isValidForm() === false}
-        />
-
-        <Button
-          class="button reset"
-          text="Limpiar el formulario"
-          click={handleResetButton}
-        />
-      </form>
+      <Form
+        name={name}
+        handleName={handleName}
+        email={email}
+        handleEmail={handleEmail}
+        region={region}
+        handleRegion={handleRegion}
+        paymentType={paymentType}
+        handlePaymentType={handlePaymentType}
+        legalTerms={legalTerms}
+        handleLegalTerms={handleLegalTerms}
+        disabled={isValidForm() === false}
+        click={handleResetButton}
+      />
+      <Preview
+        paymentType={paymentType}
+        name={name}
+        email={email}
+        region={region}
+        legalTerms={legalTerms}
+      />
     </div>
   );
 };
---
+++ modulo3/07.Lifting/01.shopping-cart/src/components/Form.js
@@ -0,0 +1,87 @@
+import InputGroupCheck from "./InputGroupCheck";
+import InputGroupRadio from "./InputGroupRadio";
+import InputGroupSelect from "./InputGroupSelect";
+import InputGroupText from "./InputGroupText";
+import Button from "./Button";
+
+const Form = (props) => {
+  const handleForm = (ev) => {
+    ev.preventDefault();
+    if (props.name !== "") {
+      console.log("Enviando datos al servidor...");
+    }
+  };
+
+  return (
+    <form className="form" onSubmit={handleForm}>
+      <h2>Rellena tus datos para finalizar la compra:</h2>
+      <div className="form">
+        <InputGroupText
+          id="name"
+          text="Escribe un nombre:"
+          type="text"
+          placeholder="María García"
+          value={props.name}
+          handleInput={props.handleName}
+        />
+        <InputGroupText
+          id="email"
+          text="Escribe un email:"
+          type="email"
+          placeholder="mariagarcia@gmail.com"
+          value={props.email}
+          handleInput={props.handleEmail}
+        />
+        <InputGroupSelect
+          id="region"
+          text="Indica tu región:"
+          value={props.region}
+          handleSelect={props.handleRegion}
+        />
+        <label className="label-text">Indica tu método de pago:</label>
+        <InputGroupRadio
+          value="creditCard"
+          text="Tarjeta de crédito"
+          checked={props.paymentType === "creditCard"}
+          handleRadio={props.handlePaymentType}
+        />
+        <InputGroupRadio
+          value="cash"
+          text="Efectivo"
+          checked={props.paymentType === "cash"}
+          handleRadio={props.handlePaymentType}
+        />
+        <InputGroupRadio
+          value="cashOnDelivery"
+          text="Contra reembolso"
+          checked={props.paymentType === "cashOnDelivery"}
+          handleRadio={props.handlePaymentType}
+        />
+        <InputGroupCheck
+          id="legalTerms"
+          text="Debes aceptar nuestros términos legales para completar la compra:"
+          checked={props.legalTerms}
+          handleCheck={props.handleLegalTerms}
+        />
+      </div>
+      <Button
+        class="button"
+        text="Enviar"
+        type="submit"
+        name={props.name}
+        email={props.email}
+        paymentType={props.paymentType}
+        legalTerms={props.legalTerms}
+        disabled={props.disabled}
+      />
+
+      <Button
+        class="button reset"
+        text="Limpiar el formulario"
+        click={props.click}
+      />
+    </form>
+  );
+};
+
+export default Form;
