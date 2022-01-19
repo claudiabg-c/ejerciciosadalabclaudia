677e6f9ca4c493168b8feeddc3d31c2686e7cfa9
Claudia <claudia.berrocalgomez@gmail.com>
2022-01-19 09:14:31 +0100

    add exercises module 3 lesson 6

--- modulo3/06.Components-and-props/src/components/Logo.js
+++ modulo3/06.Components-and-props/src/components/Logo.js
@@ -1,11 +1,15 @@
 import logo from "../images/adalab-logo-155x61.png";
+import Link from "./Link";
 import "../styles/Logo.scss";
 
 const Logo = () => {
   return (
-    <a title="Adalab" href="https://adalab.es/" className="logo">
-      <img src={logo} alt="logo adalab" />
-    </a>
+    <Link
+      title="Adalab"
+      link="https://adalab.es/"
+      className="logo"
+      text={<img src={logo} alt="logo adalab" />}
+    />
   );
 };
 
