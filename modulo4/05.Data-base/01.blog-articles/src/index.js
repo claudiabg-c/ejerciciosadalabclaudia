b2e0541e2411718919a2adbb7738668f380fba6d
Claudia <claudia.berrocalgomez@gmail.com>
2022-02-04 22:57:50 +0100

    add exercises module 4 lesson 5

---
+++ modulo4/05.Data-base/01.blog-articles/src/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const Database = require("better-sqlite3");
+
+// create app server
+const app = express();
+
+// set express middlewares
+app.use(express.json());
+app.use(cors());
+
+// set template engine middlewares
+app.set("view engine", "ejs");
+
+// init express aplication
+const serverPort = 3000;
+app.listen(serverPort, () => {
+  console.log(`App listening at http://localhost:${serverPort}`);
+});
+
+const db = new Database("./articles.db", {
+  verbose: console.log,
+});
+
+app.get("/articles", (req, res) => {
+  const query = db.prepare("SELECT * FROM articles");
+  const articles = query.all();
+  res.json(articles);
+});
+
+app.get("/article/:id", (req, res) => {
+  const query = db.prepare("SELECT * FROM articles WHERE id = ?");
+  const selectedArticle = query.all(req.params.id);
+  res.json(selectedArticle);
+});
+
+app.get("/article", (req, res) => {
+  const query = db.prepare(
+    `SELECT * FROM articles WHERE title LIKE '%${req.query.titleSearch}%'`
+  );
+  const selectedTitle = query.all();
+  res.json(selectedTitle);
+});
---
+++ modulo4/05.Data-base/02.book-store/src/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const cors = require("cors");
+const Database = require("better-sqlite3");
+
+// create app server
+const app = express();
+
+// set express middlewares
+app.use(express.json());
+app.use(cors());
+
+// set template engine middlewares
+app.set("view engine", "ejs");
+
+// init express aplication
+const serverPort = 3000;
+app.listen(serverPort, () => {
+  console.log(`App listening at http://localhost:${serverPort}`);
+});
+
+const db = new Database("./books.db", {
+  verbose: console.log,
+});
+
+app.get("/books", (req, res) => {
+  const query = db.prepare("SELECT * FROM books ORDER BY title");
+  const books = query.all();
+  res.json(books);
+});
+
+app.get("/books/minprice=:minprice", (req, res) => {
+  const query = db.prepare("SELECT * FROM books WHERE price > ?");
+  const priceRangeBooks = query.all(req.params.minprice);
+  res.json(priceRangeBooks);
+});
+
+app.get("/books/inStock", (req, res) => {
+  const query = db.prepare("SELECT * FROM books WHERE stock = ?");
+  const inStock = query.all("Si");
+  res.json(inStock);
+});
+
+app.get("/books/paperbook/instock", (req, res) => {
+  const query = db.prepare(
+    "SELECT * FROM books WHERE stock = ? AND format = ?"
+  );
+  const paperAndStock = query.all("Si", "Papel");
+  res.json(paperAndStock);
+});
+
+app.get("/book/:id", (req, res) => {
+  const query = db.prepare("SELECT * FROM books WHERE id = ?");
+  const bookById = query.get(req.params.id);
+  res.json(bookById);
+});
+
+app.get("/books/1-3", (req, res) => {
+  const query = db.prepare("SELECT * FROM books ORDER BY title LIMIT 3");
+  const books1To3 = query.all();
+  res.json(books1To3);
+});
+
+app.get("/books/3-5", (req, res) => {
+  const query = db.prepare(
+    "SELECT * FROM books ORDER BY title LIMIT 3 OFFSET 2"
+  );
+  const books3To5 = query.all();
+  res.json(books3To5);
+});
